f647518b148d3a7c66079027760a12b3932cd04d
Your Name <you@example.com>
2024-03-19 00:28:36 +0530

    first commit

---
+++ 1.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+
+#create a window
+root = tk.Tk();
+width,height = 400,400
+dis_width = root.winfo_screenwidth()
+dis_heigth = root.winfo_screenheight()
+
+left = int(dis_width/2 - width/2)
+top = int(dis_heigth/2 - height/2)
+root.geometry(f'{width}x{height}+{left}+{top}')  #width*heigth + LEFT+TOP
+
+root.title("hello world")
+root.resizable(True,False); #only can resizeable from laft to rigth 
+root.iconbitmap('one-finger-click-black-hand-symbol_icon-icons.com_64350.ico')
+#run the window
+root.mainloop();
+print("Hello")
